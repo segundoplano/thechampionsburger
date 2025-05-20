aee57d654ff9132e0f6b7cc568698d7acdaecd89
aleexrguez <alessandro.rodriguezrojas@gmail.com>
2025-05-20 11:34:10 +0200

    Version corregida con output static 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import clerk from "@clerk/astro";
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(),clerk()],
-  adapter: node({ mode: 'standalone' }),
   output: 'static',
 });
